316167529b1d1a4642921c380320a9843e13d8a8
Tobias Gruetzmacher <tobias-git@23.gs>
2008-09-27 01:30:17 +0200

    Support multiple hash types.
    Instead of just supporting MD5, this tool can now use any hash OpenSSL
    supports.

--- hash_search.c
+++ hash_search.c
@@ -1,9 +1,10 @@
-/* hash_search.c: partially reverse MD5 hashes by finding bytes to add
+/* hash_search.c: partially reverse MD5 or other hashes by finding bytes to add
  * to an existing file so that its MD5 hash begin with a specified prefix
  *
  * link with -lcrypto   (use libcrypto, supplied by OpenSSL)
  * 
  * Copyright (C) 2003, Seth Schoen
+ * Copyright (C) 2008, Tobias Gruetzmacher
  *
  * Permission is granted to any person obtaining a copy of this program
  * to deal in the program without restriction.
@@ -14,7 +15,7 @@
  *
  * Thanks to Aaron Swartz for testing on ppc.  Seemingly endian-safe. */
 
-#include <openssl/md5.h>
+#include <openssl/evp.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -23,21 +24,19 @@
 /* block size for file reads */
 #define SIZE 16384
 
-#define SAVE_STATE memcpy(dup_md5_state, md5_state, sizeof(MD5_CTX))
-#define RESTORE_STATE memcpy(md5_state, dup_md5_state, sizeof(MD5_CTX))
-
 void usage(int ret)
 {
-	fputs("usage: hash_search [-b <bits>] [-l] hexdigits\n"
-			"\t-b <bits>  number of bits to search (default: 24)\n"
-			"\t-l         just list all possible matches, don't output modified file\n", stderr);
+	fputs("usage: hash_search [-b <bits>] [-d <digest>] [-l] hexdigits\n"
+			"\t-b <bits>    number of bits to search (default: 24)\n"
+			"\t-l           just list all possible matches, don't output modified file\n"
+			"\t-d <digest>  use another digest instead of MD5\n", stderr);
 	exit(ret);
 }
 
-void print_result(FILE *f, unsigned char *result){
+void print_result(FILE *f, unsigned char *result, int len){
 	int i;
 
-	for (i = 0; i < 16; i++) fprintf(f, "%02x", result[i]);
+	for (i = 0; i < len; i++) fprintf(f, "%02x", result[i]);
 }
 
 size_t reliable_write(int fd, void *buf, size_t count){
@@ -80,13 +79,16 @@ int main(int argc, char *argv[]){
 	unsigned long long *new_byte;
 	int L, bits, ch, shift, count = 0;
 	unsigned long long max_search = 1 << 24;
-	char buf[SIZE];
-	char make_matching = 1;
-	unsigned char result[16];
-	MD5_CTX *md5_state, *dup_md5_state;
+	char buf[SIZE], make_matching = 1;
+	unsigned char result[EVP_MAX_MD_SIZE];
+	unsigned int result_len;
+	EVP_MD_CTX hash_state, dup_hash_state;
+	const EVP_MD *md = EVP_md5();
 	ssize_t n;
 
-	while ((ch = getopt(argc, argv, "b:l")) != -1) {
+	OpenSSL_add_all_digests();
+
+	while ((ch = getopt(argc, argv, "b:d:l")) != -1) {
 		switch (ch) {
 			case 'b':
 				shift = atol(optarg);
@@ -99,6 +101,14 @@ int main(int argc, char *argv[]){
 					usage(1);
 				}
 				break;
+			case 'd':
+				md = EVP_get_digestbyname(optarg);
+
+				if(!md) {
+					printf("Unknown message digest %s\n", optarg);
+					usage(1);
+				}
+
 			case 'l':
 				make_matching = 0;
 				break;
@@ -117,24 +127,25 @@ int main(int argc, char *argv[]){
 	bits = get_value(argv[optind], s);
 
 	/* allocate memory for hash state */
-	md5_state = (MD5_CTX *)malloc(sizeof(MD5_CTX));
-	dup_md5_state = (MD5_CTX *)malloc(sizeof(MD5_CTX));
+	EVP_MD_CTX_init(&hash_state);
+	EVP_MD_CTX_init(&dup_hash_state);
 	new_byte = (unsigned long long *)malloc(sizeof(unsigned long long *));
 
 	/* initialize hash */
-	MD5_Init(md5_state);
+	EVP_DigestInit_ex(&hash_state, md, NULL);
+	EVP_DigestInit_ex(&dup_hash_state, md, NULL);
 
 	/* hash the existing file */
 	fprintf(stderr, "reading file to hash from stdin...");
 	if (isatty(0)){
 		fprintf(stderr, "\n");
 		while ((n = read(0, buf, SIZE))) {
-			MD5_Update(md5_state, buf, n);
+			EVP_DigestUpdate(&hash_state, buf, n);
 			if (make_matching) reliable_write(1, buf, n);
 		}
 	} else {
 		while ((n = read(0, buf, SIZE))) {
-			MD5_Update(md5_state, buf, n);
+			EVP_DigestUpdate(&hash_state, buf, n);
 			if (make_matching) reliable_write(1, buf, n);
 			/* progress indicator */
 			if (!((count++)%256)) fprintf(stderr, ".");
@@ -144,18 +155,18 @@ int main(int argc, char *argv[]){
 
 	/* announce the start of the search */
 	fprintf(stderr, "beginning search (original hash = ");
-	SAVE_STATE;
-	MD5_Final(result, md5_state);
-	print_result(stderr, result);
-	RESTORE_STATE;
+	EVP_MD_CTX_copy(&dup_hash_state, &hash_state);
+	EVP_DigestFinal_ex(&dup_hash_state, result, &result_len);
+	print_result(stderr, result, result_len);
+	EVP_MD_CTX_cleanup(&dup_hash_state);
 	fprintf(stderr, ")\nsearching 0 to %#llx ... ", max_search);
 
 	/* do the search */
 	for (*new_byte = 0; *new_byte < max_search; (*new_byte)++){
-		SAVE_STATE;
+		EVP_MD_CTX_copy(&dup_hash_state, &hash_state);
 
-		MD5_Update(md5_state, (char *)new_byte, sizeof(int));
-		MD5_Final(result, md5_state);
+		EVP_DigestUpdate(&dup_hash_state, (char *)new_byte, sizeof(int));
+		EVP_DigestFinal_ex(&dup_hash_state, result, &result_len);
 		if (!memcmp(result, s, bits/8)) {
 			/* just one last nibble? */
 			if ((bits%8 == 0) || ((result[bits/8] & 0xf0) == (s[bits/8] & 0xf0))){
@@ -163,24 +174,25 @@ int main(int argc, char *argv[]){
 				/* goal is to output an actual matching file */
 				fprintf(stderr, "found match!\n");
 				fprintf(stderr, "new hash is ");
-				print_result(stderr, result);
+				print_result(stderr, result, result_len);
 				fprintf(stderr, "\n");
 				reliable_write(1, new_byte, 4);
 				close(1);
 				exit(0);
 			} else {
 				/* goal is to display all possible matches */
-				print_result(stdout, result);
+				print_result(stdout, result, result_len);
 				fprintf(stdout, " bytes %#llx\n", *new_byte);
 			}
 			}
 		}
 
-		RESTORE_STATE;
+		EVP_MD_CTX_cleanup(&dup_hash_state);
 	}
 
 	/* free memory */
-	free(md5_state); free(dup_md5_state); free(s);
+	EVP_MD_CTX_cleanup(&hash_state);
+	free(s);
 
 	if (make_matching) fprintf(stderr, "no match found.\n");
 	/* if the goal was to output a matching file, then fail if we got
